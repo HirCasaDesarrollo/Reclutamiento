de44b952379472d1071a5f87fd77da877af478d8
Isaac Bicri Ortiz Martinez <isaac.bicri@gmail.com>
2023-01-21 14:02:54 -0600

    Inicio del proyecto

---
+++ PruebaTecnica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaTecnica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
