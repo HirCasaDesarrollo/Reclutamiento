9f888807f9c225809b44f6a5e6ce456ab425b34f
manuelml99 <manuelml150999@gmail.com>
2023-02-10 10:12:50 -0600

    Examen Tecnico

---
+++ Prueba/Prueba/Models/ViewModels/TablaPagos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba.Models.ViewModels
+{
+    public class TablaPagos
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public decimal MontoPago { get; set; }
+        public int Aplicado { get; set; }
+        public DateTime FechaPago { get; set; }
+    }
+}
