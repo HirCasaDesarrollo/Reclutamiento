3d5de55c07327b83b4929c498d8f579aed5b013c
MemoRodz <gmo.rodriguez@gmail.com>
2023-03-25 05:09:42 -0600

    Update: Prueba-Jr

---
+++ test-jr/src/views/Progress.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="progress-page">
+        <h1>Progress</h1>
+        <p>Este es la progress page.</p>
+    </main>
+</template>
---
+++ test-jr/src/views/SettingsPg.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="settings-page">
+        <h1>Settings</h1>
+        <p>Este es la settings page.</p>
+    </main>
+</template>
